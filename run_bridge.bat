a214f1a0a36c2291f7a97b7316167a525f473fe7
Max Infeld <maxinfeld@gmail.com>
2026-09-16 15:16:19 -0700

    Add run_bridge.bat

---
+++ run_bridge.bat
@@ -0,0 +1,8 @@
+@echo off
+set PYTHON_EXE=%LOCALAPPDATA%\Programs\Python\Python312\python.exe
+if not exist "%PYTHON_EXE%" (
+    set PYTHON_EXE=python
+)
+cd /d "%~dp0"
+"%PYTHON_EXE%" bridge.py
+pause
